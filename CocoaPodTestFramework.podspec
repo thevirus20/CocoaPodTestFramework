21b6bd04261309a2693afbfdfbeae1e9b24bf276
Azam Shaikh <thevirus20@gmail.com>
2018-01-21 01:47:15 +0530

    Network Call Added

--- CocoaPodTestFramework.podspec
+++ CocoaPodTestFramework.podspec
@@ -1,6 +1,6 @@
 Pod::Spec.new do |s|
   s.name         = "CocoaPodTestFramework"
-  s.version      = "0.0.1"
+  s.version      = "0.0.2"
   s.summary      = "Some some CocoaPodTestFramework."
 
   s.description  = "Some some CocoaPodTestFramework CocoaPodTestFrameworkCocoaPodTestFrameworkCocoaPodTestFramework"
--- CocoaPodTestFramework/RandomGenerator.swift
+++ CocoaPodTestFramework/RandomGenerator.swift
@@ -7,20 +7,76 @@
 //
 
 import Foundation
+//import CoreLocation
+import Alamofire
+import SwiftyJSON
 
-public class RandomGenerator{
-    public init(){}
+@objc public class RandomGenerator: NSObject{
     
-    public func str() -> String{
+    let WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather"
+    let APP_ID = "_"
+
+    override public init(){
+        super.init()
+        
+        //locationManager.delegate = self
+        //locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
+        //locationManager.requestWhenInUseAuthorization()
+        //locationManager.startUpdatingLocation()
+        
+    }
+    
+    @objc public func str() -> String{
         return UUID().uuidString
     }
     
-    public func intt2() -> Int {
+    @objc public func intt_3() -> Int {
         return Int(arc4random())
     }
     
     public func anotherMethod() {
         print("nothing")
     }
+    
+    
+    //MARK: - Networking
+    /***************************************************************/
+    
+    //Write the getWeatherData method here:
+    
+    func getWeatherData(url: String, parameters: [String: String]) {
+        
+        Alamofire.request(url, method: .get, parameters: parameters).responseJSON {
+            response in
+            if response.result.isSuccess {
+                
+                print("Success! Got the weather data")
+                let weatherJSON : JSON = JSON(response.result.value!)
+                
+                
+                print(weatherJSON)
+                
+                //self.updateWeatherData(json: weatherJSON)
+                
+            }
+            else {
+                print("Error \(String(describing: response.result.error))")
+                //self.cityLabel.text = "Connection Issues"
+            }
+        }
+        
+    }
+    
+    //Write the userEnteredANewCityName Delegate method here:
+    
+    
+    @objc public func userEnteredANewCityName1(_ city: String) {
+        
+        let params : [String : String] = ["q" : city, "appid" : APP_ID]
+        
+        getWeatherData(url: WEATHER_URL, parameters: params)
+        
+    }
+
 }
 
